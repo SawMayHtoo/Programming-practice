d7446f04d2c32c1e92118f73591c9ccbc2495ed1
SawMayHtoo <sawmayhtoo@outlook.com>
2026-01-29 07:02:21 +0700

    First commit

---
+++ variables.py
@@ -0,0 +1,49 @@
+#Day 2: 30 Days of python programming
+first_name = "saw"
+last_name = "May"
+full_name = "Saw May"
+country = "Myanmar"
+city = "Hpa-am"
+age = 20
+is_married = False
+is_true = True
+is_light_on = False
+first_name, last_name, full_name, country, city, age, is_married, is_true, is_light_on = "saw", "May", "Saw May", "Myanamar", "Hpa-an", "20", False, True, False
+
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+
+print(len(first_name))
+print(len(last_name))
+
+num_one = 5
+num_two = 4
+total = num_one + num_two
+diff = num_two - num_one
+product = num_two * num_one
+dividsion = num_one / num_two
+remainder = num_two % num_one
+exp = num_two ** num_one
+floor_division = num_one // num_two
+
+#caculate the Circle
+radius = 30
+pi = 3.14
+area_of_circle = pi* radius**2
+circum_of_circle = 2 * pi * radius
+radius_from_user = float(input("Insert value of radius: "))
+caculate_the_area = pi* radius_from_user**2
+
+#built-in input function to get the information
+First_name = input("Enter your First name: ")
+Last_name = input("Enter your Last Name: ")
+Country = input("Enter your country: ")
+Age = input ("Enter your age: ")
+help('keywords')
